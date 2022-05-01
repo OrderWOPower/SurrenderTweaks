8a595cf24408a12a4883e49595f1bb3c567cd3e7
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2022-05-02 02:07:43 +0800

    Add files via upload

--- SurrenderTweaksSettings.cs
+++ SurrenderTweaksSettings.cs
@@ -15,23 +15,23 @@ namespace SurrenderTweaks
         public override string FormatType => "json2";
 
         [SettingPropertyFloatingInteger("{=SurrenderTweaks18}Chance of Bribe", 0.0f, 10.0f, "0.0", Order = 0, RequireRestart = false, HintText = "{=SurrenderTweaks19}Multiplier for chance of bribe. Default is 1.0.")]
-        [SettingPropertyGroup("{=SurrenderTweaks17}Multipliers")]
+        [SettingPropertyGroup("{=SurrenderTweaks17}Multipliers", GroupOrder = 0)]
         public float BribeChanceMultiplier { get; set; } = 1.0f;
 
         [SettingPropertyFloatingInteger("{=SurrenderTweaks20}Chance of Surrender", 0.0f, 10.0f, "0.0", Order = 1, RequireRestart = false, HintText = "{=SurrenderTweaks21}Multiplier for chance of surrender. Default is 1.0.")]
-        [SettingPropertyGroup("{=SurrenderTweaks17}Multipliers")]
+        [SettingPropertyGroup("{=SurrenderTweaks17}Multipliers", GroupOrder = 0)]
         public float SurrenderChanceMultiplier { get; set; } = 1.0f;
 
         [SettingPropertyFloatingInteger("{=SurrenderTweaks22}Settlement Nutrition Bonus", 0.0f, 10.0f, "0.0", Order = 2, RequireRestart = false, HintText = "{=SurrenderTweaks23}Multiplier for a besieged settlement's nutrition bonus. Default is 1.0.")]
-        [SettingPropertyGroup("{=SurrenderTweaks17}Multipliers")]
+        [SettingPropertyGroup("{=SurrenderTweaks17}Multipliers", GroupOrder = 0)]
         public float NutritionBonusMultiplier { get; set; } = 1.0f;
 
         [SettingPropertyFloatingInteger("{=SurrenderTweaks24}Settlement Starvation Penalty", 0.0f, 10.0f, "0.0", Order = 3, RequireRestart = false, HintText = "{=SurrenderTweaks25}Multiplier for a besieged settlement's starvation penalty. Default is 1.0.")]
-        [SettingPropertyGroup("{=SurrenderTweaks17}Multipliers")]
+        [SettingPropertyGroup("{=SurrenderTweaks17}Multipliers", GroupOrder = 0)]
         public float StarvationPenaltyMultiplier { get; set; } = 1.0f;
 
         [SettingPropertyFloatingInteger("{=SurrenderTweaks26}Bribe Amount", 0.0f, 10.0f, "0.0", Order = 4, RequireRestart = false, HintText = "{=SurrenderTweaks27}Multiplier for the bribe amount. Default is 1.0.")]
-        [SettingPropertyGroup("{=SurrenderTweaks17}Multipliers")]
+        [SettingPropertyGroup("{=SurrenderTweaks17}Multipliers", GroupOrder = 0)]
         public float BribeAmountMultiplier { get; set; } = 1.0f;
 
         [SettingPropertyInteger("{=SurrenderTweaks29}Lord Bribe Cooldown", 1, 10, "0", Order = 0, RequireRestart = false, HintText = "{=SurrenderTweaks30}Number of days cooldown for lord bribes. Default is 10.")]
